a52f02f3ca90141a5343ab1579c41ce4c4a488b7
Dorcas Yoila Gemu <missgemu@gmail.com>
2022-03-11 17:11:07 +0100

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
+#include <std.h>
 /**
- * main - Entry point
- * Return: Always 0 (Success)
- */
+*main - Entry point
+*My hello, printf task
+*Return: return 1 after program execution
+*/
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a place of art,\n");
 return (0);
-}
+} 
