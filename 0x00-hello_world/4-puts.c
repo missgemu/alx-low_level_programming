e79e0802de38c5702b1a2c8be68eba1922aca8fb
Dorcas Yoila Gemu <missgemu@gmail.com>
2022-03-11 17:14:41 +0100

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
-*main - entry point
-*
-*Return: 0 after printing the function
-*/
+ * main - Entry Point
+ *
+ *Return: Always 0 (Success)
+ **/
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-} 
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
