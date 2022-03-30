2f27e259f165c6124e46bd820594b27d05dc317d
Dorcas Yoila Gemu <missgemu@gmail.com>
2022-03-30 16:23:09 +0100

    raised to the power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,4 +1,4 @@
-#include "maiin.h"
+#include "main.h"
 
 /**
  * _pow_recursion - returns the value of x raised to the power of y
@@ -15,3 +15,4 @@ int _pow_recursion(int x, int y)
 		return (1);
 	return (x * _pow_recursion(x, y - 1));
 }
+
