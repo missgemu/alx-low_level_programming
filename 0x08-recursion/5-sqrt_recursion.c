8cfcc356857eb04de48f774a0cd4d5ce953d758c
Dorcas Yoila Gemu <missgemu@gmail.com>
2022-03-30 16:46:35 +0100

    natural square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,7 +3,7 @@
 int actual_sqrt_recursion(int n, int i);
 
 /**
- * _sqr_recursion - returns the natural square root of a number
+ * _sqrt_recursion - returns the natural square root of a number
  * @n: number to calculate the square root of
  *
  * Return: the resulting square root
@@ -18,7 +18,7 @@ int _sqrt_recursion(int n)
 /**
  * actual_sqrt_recursion - recurses to find the natural
  * square root of a number
- * @n: number to calculate the square root of
+ * @n: number to calculate the sqaure root of
  * @i: iterator
  *
  * Return: the resulting square root
@@ -30,4 +30,5 @@ int actual_sqrt_recursion(int n, int i)
 	if (i * i == n)
 		return (i);
 	return (actual_sqrt_recursion(n, i + 1));
-} 
+}
+
